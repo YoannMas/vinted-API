923f0e8e6513f5734b5140a7e31f19e67f20a0b2
YoannMas <yoann.masson.dev@gmail.com>
2021-02-10 13:51:02 +0100

    add port

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ app.use(cors());
 app.use(formidable());
 
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_PUBLIC_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET_KEY,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_PUBLIC_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
 });
 
 const userRoutes = require("./routes/user");
@@ -27,9 +27,9 @@ const offerRoutes = require("./routes/offer");
 app.use(offerRoutes);
 
 app.all("*", (req, res) => {
-    res.status(404).json("Page not found");
+  res.status(404).json("Page not found");
 });
 
-app.listen(process.env.PORT, () => {
-    console.log("Server has started");
+app.listen(process.env.PORT || 3100, () => {
+  console.log("Server has started");
 });
