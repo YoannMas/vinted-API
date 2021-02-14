82c62794082d14034a32b03253882e6f4094f989
YoannMas <yoann.masson.dev@gmail.com>
2021-02-14 17:24:53 +0100

    add product_pictures

--- routes/offer.js
+++ routes/offer.js
@@ -6,7 +6,7 @@ const isAuthenticated = require("../middleware/isAuthenticated");
 const cloudinary = require("cloudinary").v2;
 
 router.post("/offer/publish", isAuthenticated, async (req, res) => {
-  console.log(req.fields);
+  console.log(req);
   try {
     if (req.fields.title.length < 50) {
       if (req.fields.description.length < 500) {
@@ -29,6 +29,7 @@ router.post("/offer/publish", isAuthenticated, async (req, res) => {
               folder: `/vinted/offer/${newOffer._id}`,
             });
             newOffer.product_image = result;
+            newOffer.product_pictures.push(result);
           }
           await newOffer.save();
           res.status(200).json(newOffer);
@@ -50,64 +51,63 @@ router.put("/offer/modify", isAuthenticated, async (req, res) => {
   try {
     const offerToModify = await Offer.findById(req.query.id);
     if (offerToModify) {
-      if (req.fields.title.length < 50) {
+      if (req.fields.title) {
+        if (req.fields.title.length < 50) {
+          offerToModify.product_name = req.fields.title;
+        } else {
+          res.status(400).json({ message: "Name must be lower than 50 characters" });
+        }
+      }
+      if (req.fields.description) {
         if (req.fields.description.length < 500) {
-          if (req.fields.price < 100000) {
-            if (req.fields.title) {
-              offerToModify.product_name = req.fields.title;
-            }
-            if (req.fields.description) {
-              offerToModify.product_description = req.fields.description;
-            }
-            if (req.fields.price) {
-              offerToModify.product_price = req.fields.price;
-            }
-
-            const details = offerToModify.product_details;
-            for (i = 0; i < details.length; i++) {
-              if (details[i].MARQUE) {
-                if (req.fields.brand) {
-                  details[i].MARQUE = req.fields.brand;
-                }
-              }
-              if (details[i].TAILLE) {
-                if (req.fields.size) {
-                  details[i].TAILLE = req.fields.size;
-                }
-              }
-              if (details[i].ÉTAT) {
-                if (req.fields.condition) {
-                  details[i].ÉTAT = req.fields.condition;
-                }
-              }
-              if (details[i].COULEUR) {
-                if (req.fields.color) {
-                  details[i].COULEUR = req.fields.color;
-                }
-              }
-              if (details[i].EMPLACEMENT) {
-                if (req.fields.location) {
-                  details[i].EMPLACEMENT = req.fields.location;
-                }
-              }
-            }
-            if (req.files.picture) {
-              const result = await cloudinary.uploader.upload(req.files.picture.path, {
-                folder: `/vinted/offer/${offerToModify._id}`,
-              });
-              offerToModify.product_image = result;
-            }
-            await offerToModify.save();
-            res.status(200).json(await offerToModify.populate("owner", "account").execPopulate());
-          } else {
-            res.status(400).json({ message: "Price must be lower than 100000" });
-          }
+          offerToModify.product_description = req.fields.description;
         } else {
           res.status(400).json({ message: "Description must be lower than 500 characters" });
         }
-      } else {
-        res.status(400).json({ message: "Name must be lower than 50 characters" });
       }
+      if (req.fields.price) {
+        if (req.fields.price < 100000) {
+          offerToModify.product_price = req.fields.price;
+        } else {
+          res.status(400).json({ message: "Price must be lower than 100000" });
+        }
+      }
+      const details = offerToModify.product_details;
+      for (i = 0; i < details.length; i++) {
+        if (details[i].MARQUE) {
+          if (req.fields.brand) {
+            details[i].MARQUE = req.fields.brand;
+          }
+        }
+        if (details[i].TAILLE) {
+          if (req.fields.size) {
+            details[i].TAILLE = req.fields.size;
+          }
+        }
+        if (details[i].ÉTAT) {
+          if (req.fields.condition) {
+            details[i].ÉTAT = req.fields.condition;
+          }
+        }
+        if (details[i].COULEUR) {
+          if (req.fields.color) {
+            details[i].COULEUR = req.fields.color;
+          }
+        }
+        if (details[i].EMPLACEMENT) {
+          if (req.fields.location) {
+            details[i].EMPLACEMENT = req.fields.location;
+          }
+        }
+      }
+      if (req.files.picture) {
+        const result = await cloudinary.uploader.upload(req.files.picture.path, {
+          folder: `/vinted/offer/${offerToModify._id}`,
+        });
+        offerToModify.product_pictures.push(result);
+      }
+      await offerToModify.save();
+      res.status(200).json(await offerToModify.populate("owner", "account").execPopulate());
     } else {
       res.status(400).json({ message: "Invalid ID" });
     }
